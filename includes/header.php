ff73e7070294ecdf4edc5e904caddd53f7b6b23d
Nahuel Achille <nahuelachille@gmail.com>
2026-04-26 02:01:25 -0300

    Inicio proyecto

---
+++ includes/header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../public/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" rel="stylesheet">
+</head>
+<body>
+
+<!-- Navbar principal -->
+<nav class="navbar navbar-expand navbar-dark " style="background-color: #055160;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+    <i class="bi bi-shield-lock-fill me-1"></i>Admin
+    </a>
+
+    
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+    <li class="nav-item text-center">
+        <a class="nav-link" href="#" title="Tareas">
+            <i class="bi bi-clipboard"></i>
+          
+        </a>
+    </li>
+    <li class="nav-item text-center">
+        <a class="nav-link" href="#" title="Etiquetas">
+            <i class="bi bi-tag"></i>
+          
+        </a>
+    </li>
+    <li class="nav-item text-center">
+        <a class="nav-link" href="#" title="Usuarios">
+            <i class="bi bi-people"></i>
+      
+        </a>
+    </li>
+    <li class="nav-item text-center">
+        <a class="nav-link active" href="#" title="Admin">
+            <i class="bi bi-person-circle"></i>
+          
+        </a>
+    </li>
+</ul>
+    </div>
+  </div>
+</nav>
---
+++ pages/index.php
@@ -0,0 +1,10 @@
+<?php include '../includes/header.php'; ?>
+
+<div class="container mt-4">
+    <h1>Bienvenido al Dashboard</h1>
+    <p>Este es el panel de administración donde puedes gestionar tu sitio web.</p>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
