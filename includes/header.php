6341c3a4141abb822ee3ff085ffaf78be06cfa97
rossier <rossiermain@gmail.com>
2026-04-26 04:16:06 -0300

    cambios del nav RossierMain

--- includes/header.php
+++ includes/header.php
@@ -2,50 +2,49 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="../public/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;**600**;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" rel="stylesheet">
 </head>
 <body>
 
-<!-- Navbar principal -->
-<nav class="navbar navbar-expand navbar-dark " style="background-color: #055160;">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">
-    <i class="bi bi-shield-lock-fill me-1"></i>Admin
+<nav class="navbar-expand fixed-top " style="background-color: #055160;">
+  <div class="container-fluid conteiner-nav d-flex justify-content-between align-items-center gap-3">
+    <a class="text-white nav-link fw-semibold fs-5" href="#">
+        <i class="bi bi-shield-lock-fill me-1 nav-icon"></i>Admin
     </a>
-
     
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav ms-auto">
-    <li class="nav-item text-center">
-        <a class="nav-link" href="#" title="Tareas">
-            <i class="bi bi-clipboard"></i>
-          
-        </a>
-    </li>
-    <li class="nav-item text-center">
-        <a class="nav-link" href="#" title="Etiquetas">
-            <i class="bi bi-tag"></i>
-          
-        </a>
-    </li>
-    <li class="nav-item text-center">
-        <a class="nav-link" href="#" title="Usuarios">
-            <i class="bi bi-people"></i>
-      
-        </a>
-    </li>
-    <li class="nav-item text-center">
-        <a class="nav-link active" href="#" title="Admin">
-            <i class="bi bi-person-circle"></i>
-          
-        </a>
-    </li>
-</ul>
+    <div class="content-fluid" id="navbarNav">
+      <ul class="navbar-nav gap-3">
+            <li class="text-center">
+                <a class="text-white" href="#" title="Tareas">
+                    <i class="bi bi-clipboard-check nav-icon"></i>
+            
+                </a>
+            </li>
+            <li class="text-center">
+                <a class="text-white" href="#" title="Etiquetas">
+                    <i class="bi bi-tags-fill nav-icon"></i>
+            
+                </a>
+            </li>
+            <li class="text-center">
+                <a class="text-white" href="#" title="Usuarios">
+                    <i class="bi bi-people-fill nav-icon"></i>
+        
+                </a>
+            </li>
+            <li class="text-center">
+                <a class="text-white" href="#" title="Admin">
+                    <i class="bi bi-person-circle nav-icon"></i>
+            
+                </a>
+            </li>
+       </ul>
     </div>
   </div>
 </nav>
