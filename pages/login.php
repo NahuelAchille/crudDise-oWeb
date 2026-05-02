488e68eff9e2e7850df70447cfee9cdb49d5202f
rossier <rossiermain@gmail.com>
2026-05-02 17:42:54 -0300

    modificaciones al css y html visual del login

--- pages/login.php
+++ pages/login.php
@@ -6,32 +6,42 @@
   <title>Login - Panel de Administración</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" rel="stylesheet">
-  <link rel="stylesheet" href="/CRUDDISEÑOWEB/public/css/style.css">
+  <link rel="stylesheet" href="../public/css/style.css">
 </head>
-<body id="bodyLogin">
-    <div class="d-flex flex-column justify-content-center align-items-center vh-100">
+<body id="bodyLogin" class="d-flex flex-column" >
+    <div class="d-flex flex-column align-items-center flex-grow-1 login-container">
 
     <div class="text-center mb-4">
-      <i class="bi bi-shield-lock" id="shield"></i>
+      <i class="bi bi-shield-lock-fill" id="shield"></i>
       <h3 class="text-center text-white">Panel de Administración</h3>
       <p class="text-center text-light" >Por favor ingrese sus credenciales para continuar</p>
     </div>
 
     <div class="card p-4 shadow" id="cardForm">
       <form method="POST" action="login.php">
-        <div class="mb-3">
+        <div class="mb-3 input-info">
           <label for="email" class="form-label">Correo electrónico</label>
-          <input type="email" class="form-control" id="email" placeholder="Usuario@ejemplo.com" required>
+          <div class="input-group shadow-sm">
+            <span class="input-group-text" id="basic-addon1">
+              <i class="bi bi-envelope-fill"></i>
+            </span>
+            <input type="email" class="form-control shadow-none" id="email" placeholder="Usuario@ejemplo.com" required>
+          </div>
         </div>
-        <div class="mb-3 position-relative">
+        <div class="mb-3 input-info">
           <label for="password" class="form-label">Contraseña</label>
-          <input type="password" class="form-control" id="password" placeholder="********" required>
-      
-          <span class="position-absolute top-50 end-0 translate-middle-y me-3" style="cursor:pointer;">
-            <i class="bi bi-eye" id="togglePassword"></i>
+            <div class="input-group shadow-sm">
+              <span class="input-group-text" id="basic-addon">
+                <i class="bi bi-lock-fill"></i>
+              </span>
+              <input type="password" class="form-control border-end-0 shadow-none" id="password" placeholder="Contraseña" required>
+              <span class="input-group-text" id="togglePassword" style="cursor: pointer;">
+                    <i class="bi bi-eye-fill"></i>
+              </span>
+            </div>
           </span>
         </div>
-        <button type="submit" class="btn btn-primary w-100" style="background-color: #055160">Ingresar</button>
+        <button type="submit" class="btn btn w-100" style="background-color: #087990; color:white">Ingresar</button>
       </form>
     </div>
   </div>
