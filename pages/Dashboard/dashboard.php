59870fb844772a8c62b1f31986dacf2dbe46cd9e
rossier <rossiermain@gmail.com>
2026-05-12 15:29:11 -0300

    Agrega cambios ui a listUser.php

---
+++ pages/Dashboard/dashboard.php
@@ -0,0 +1,77 @@
+
+<div class="d-block d-md-none">
+    <?php include '../../includes/header.php'; ?>
+</div>
+
+<div class="d-none d-md-block">
+    <?php include '../../includes/sideBar.php'; ?>
+</div>
+    <style>
+        @media (min-width: 768px) {
+            .main-content-desktop {
+                margin-left: 20rem !important;
+                display: block !important;
+            }
+        }
+    </style>
+    <main class="main-content-desktop">
+        <div class="p-2 shadow" style="color: #055160;">
+            <article class="d-flex gap-2 align-items-center">
+                <i class="bi bi-clipboard-check nav-icon"></i>
+                <span>Dashboard</span>
+            </article>
+        </div>
+        <ul class="navbar-nav d-flex flex-column flex-md-row p-3 flex-wrap">
+            <li class="col-11 col-lg-5 shadow rounded-3 m-3 p-4 fs-3 gap-3 d-flex flex-fill align-items-center">
+                    <i class="bi bi-people-fill p-3 display-4 rounded text-white" style="background-color: #FFDA6A;"></i>
+                    <span class="display-4 fw-semibold">20</span>
+                    <span class="ms-auto text-end">Total de Usuarios</span>
+            </li>
+            <li class="col-11 col-lg-5 shadow rounded-3 m-3 p-4 fs-3 gap-3 d-flex flex-fill align-items-center">
+                    <i class="bi bi-person-check-fill p-3 display-4 rounded text-white" style="background-color: #79DFC1;"></i>
+                    <span class="display-4 fw-semibold">20</span>
+                    <span class="ms-auto text-end">Usuarios Activos</span>
+            </li>
+            <li class="col-11 col-lg-5 shadow rounded-3 m-3 p-4 fs-3 gap-3 d-flex flex-fill align-items-center">
+                    <i class="bi bi-tags-fill p-3 display-4 rounded text-white" style="background-color: #6EDFF6;"></i>
+                    <span class="display-4 fw-semibold">20</span>
+                    <span class="ms-auto text-end">Total de Roles</span>
+            </li>
+            <li class="col-11 col-lg-5 shadow rounded-3 m-3 p-4 fs-3 gap-3 d-flex flex-fill align-items-center">
+                    <i class="bi bi-person-x-fill p-3 display-4 rounded text-white" style="background-color: #EA868F;"></i>
+                    <span class="display-4 fw-semibold">20</span>
+                    <span class="ms-auto text-end">Usuarios Inactivos</span>
+            </li>
+        </ul>
+        <div class="p-3 m-3 mb-0">
+            <div class=" p-2 rounded rounded-bottom-0 d-flex gap-3 align-items-center text-white" style="background-color: #055160;">
+                <i class="bi bi-clock-history fs-3"></i>
+                <span>
+                    Últimos usuarios Registrados
+                </span>
+                <button class="btn text-white ms-auto fw-semibold" style="background-color: #20C997;">Ver Todos</button>
+            </div>  
+            <div class="table-responsive shadow-sm rounded-bottom">  
+                <table class="table table-striped-columns m-0">
+                    <thead>
+                            <tr>
+                            <th>Nombre</th>
+                            <th>Rol</th>
+                            <th>Email</th>
+                            <th>Fecha de Registro</th>
+                            </tr>
+                    </thead>
+                    <tbody>
+                            <tr>
+                            <td>1</td>
+                            <td>Admin</td>
+                            <td>admin@mail.com</td>
+                            <td>21/04/2026 11:04</td>
+                            </tr>
+                    </tbody>
+                </table>
+            </div>
+    </main>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
--- pages/Users/listUser.php
+++ pages/Users/listUser.php
@@ -1,12 +1,61 @@
-<?php include '../includes/header.php'; ?>
-
-<div class="sub-bar">
-  <div class="container-fluid d-flex align-items-center gap-2">
-    <i class="bi bi-clipboard-check"></i>
-    <span class="fw-semibold" id="dashboard">Dashboard</span>
-  </div>
+<div class="d-block d-md-none">
+    <?php include '../../includes/header.php'; ?>
 </div>
 
+<div class="d-none d-md-block">
+    <?php include '../../includes/sideBar.php'; ?>
+</div>
+    <style>
+        @media (min-width: 768px) {
+            .main-content-desktop {
+                margin-left: 20rem !important;
+                display: block !important;
+            }
+        }
+    </style>
+    <main class="main-content-desktop d-flex flex-column">
+        <div class="p-2 shadow" style="color: #055160;">
+            <article class="d-flex gap-2 align-items-center">
+                <i class="bi bi-people-fill nav-icon"></i>
+                <span>Gestión de Usuarios</span>
+            </article>
+        </div>
+        <div class="tabla-de-usuarios p-3 m-3 mb-0">
+          <div class="p-3 rounded rounded-bottom-0 d-flex gap-3 align-items-center text-white" style="background-color: #055160;">
+              <i class="bi bi-people-fill fs-2"></i>
+              <span>
+                    Listado de Usuarios
+              </span>
+              <button class="btn text-white ms-auto fw-semibold text-nowrap" style="background-color: #20C997;">+ Agregar</button>
+          </div>
+          <div class="p-3" style="background-color: #5DA9B8;">
+            <div class="input-info">
+              <label for="search" class="form-label d-block col-12 col-lg-3 m-0">
+                <div class="input-group shadow">
+                  <span class="input-group-text" id="input-icon">
+                      <i class="bi bi-search"></i>
+                  </span>
+                  <input type="text" class="form-control shadow-none" id="search" placeholder="Buscar por nombre, email o DNI">
+                </div>
+              </label>
+            </div>
+          </div>
+                <table class="table table-striped-columns m-0">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Nombre</th>
+                            <th>email</th>
+                            <th>DNI</th>
+                            <th>Rol</th>
+                            <th>Estado</th>
+                            <th>Registro</th>
+                        </tr>
+                    </thead>
+                    <?php include '../../includes/'; ?>
+                </table>
+        </div>  
+    </main>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
